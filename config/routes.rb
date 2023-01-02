1cdb81606bd40d132f30676de88dfa0717f82a8b
Udit Singh <uditsingh294@gmail.com>
2023-01-03 02:15:47 +0530

    fixed delete bug using button_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+    	member do
+    		patch :complete
+    	end
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root "todo_lists#index"
